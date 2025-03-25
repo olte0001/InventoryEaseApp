490e18512b15aaaeb757884c22c7658eae8d96b5
olte0001 <olte0001@algonquinlive.com>
2025-03-25 16:14:35 -0400

    Created Spring Boot Project
    In IntelliJ IDEA

---
+++ backend/InventoryEase/src/main/java/com/group17/inventoryease/InventoryEaseApplication.java
@@ -0,0 +1,13 @@
+package com.group17.inventoryease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryEaseApplication.class, args);
+	}
+
+}
---
+++ backend/InventoryEase/src/test/java/com/group17/inventoryease/InventoryEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group17.inventoryease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
