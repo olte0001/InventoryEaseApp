f3356ec4860e622914c6058028f96faec9c4fa3c
kvanr <vanr0039@algonquinlive.com>
2025-04-06 19:28:02 -0400

    Validate Company update

--- frontend/app/src/main/java/com/group17/inventoryease/MainActivity.java
+++ frontend/app/src/main/java/com/group17/inventoryease/MainActivity.java
@@ -2,6 +2,10 @@ package com.group17.inventoryease;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,6 +29,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        //company EditText
+        EditText companyEntry = findViewById(R.id.companyNameEntry);
+        String companyname = "";
+        //Submit company name button
+        Button submitButton = findViewById(R.id.companySubmit);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //company edittest
+                EditText companyEntry = findViewById(R.id.companyNameEntry);
+                String companyname = companyEntry.getText().toString();
+                validateCompanyId(Integer.valueOf(companyname));
+            }
+        });
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -53,12 +75,16 @@ public class MainActivity extends AppCompatActivity {
                     finish();
                 } else {
                     // TODO: Display message of invalid company identifier
+                    ((EditText) findViewById(R.id.companyNameEntry)).getText().clear();
+                    ((TextView) findViewById(R.id.companyErrorText)).setText("Error: Company name not found");
                 }
             }
 
             @Override
             public void onFailure(Call<CompanyIdResponse> call, Throwable t) {
                 // TODO: Display message that error on our side and to retry
+                ((EditText) findViewById(R.id.companyNameEntry)).getText().clear();
+                ((TextView) findViewById(R.id.companyErrorText)).setText("Error: "+t);
             }
         });
     }
